b9cf9609d635ae48fc8a2882212fb82e78045276
Noah Horwitz <00noah@gmail.com>
2019-11-01 13:24:32 -0700

    add remix button with js to reviese array and reprint in dom.

--- js/scripts.js
+++ js/scripts.js
@@ -25,8 +25,8 @@ function numberBeepBoop(numberInput) {
       beepBoopArray.push(number);
     }
   });
-  console.log("beepBoopArray: ",beepBoopArray);
 
+  console.log("beepBoopArray: ",beepBoopArray);
   return beepBoopArray;
 }
 
@@ -36,46 +36,86 @@ $(document).ready(function(){
   $("#userForm").submit(function(event){
     event.preventDefault();
 
-    setTimeout(function(){
-      alert("test");
-    }, 5000);
+    // setTimeout(function(){
+      //   alert("test");
+      // }, 0);
 
+      $(".resultSection").slideUp(500);
 
-    $(".resultSection").slideUp(500);
-    // REMOVE ALL CHILDREN OF RESULT-LIST ON NEW SUBMIT:
-    $("#resultList").children().remove();
+      // REMOVE ALL CHILDREN OF RESULT-LIST ON NEW SUBMIT:
+      $("#resultList").children().remove();
 
+      // CAPTURE USER'S NUMBER:
+      var numberInput = parseInt($("#userNumber").val());
+      var nameInput = $("#userName").val();
 
-    // CAPTURE USER'S NUMBER:
-    var numberInput = parseInt($("#userNumber").val());
-    var nameInput = $("#userName").val();
+      // CALL BUSINESS LOGIC TO RETURN ARRAY:
+      numberBeepBoop(numberInput);
 
+      console.log("beepBoopArray: ",beepBoopArray);
 
-    // CALL BUSINESS LOGIC TO RETURN ARRAY:
-    numberBeepBoop(numberInput);
-
-    // RETURN ARRAY TO DOM:
-    beepBoopArray.forEach(function(number,index){
-      if (number === "I'm sorry, Dave. I'm afraid I can't do that.") {
-        if (nameInput === "") {
-          $("#resultList").append("<li class='dave'>"+index+": "+number+"</li>");
-        }
-        else {
-          $("#resultList").append("<li class='dave'>"+index+": "+number.slice(0,11)+nameInput+number.slice(-29,number.length)+"</li>");
-        }
+      // RETURN ARRAY TO DOM:
+      function printArray(array){
+        array.forEach(function(number,index){
+          if (number === "I'm sorry, Dave. I'm afraid I can't do that.") {
+            if (nameInput === "") {
+              $("#resultList").append("<li class='dave'>"+": "+number+"</li>");
+            }
+            else {
+              $("#resultList").append("<li class='dave'>"+": "+number.slice(0,11)+nameInput+number.slice(-29,number.length)+"</li>");
+            }
+          }
+          else if (number === "Boop!") {
+            $("#resultList").append("<li class='boop'>"+": "+number+"</li>");
+          }
+          else if (number === "Beep!") {
+            $("#resultList").append("<li class='beep'>"+": "+number+"</li>");
+          }
+          else {
+            $("#resultList").append("<li class='normal'>"+": "+number+"</li>");
+          }
+        });
       }
-      else if (number === "Boop!") {
-        $("#resultList").append("<li class='boop'>"+index+": "+number+"</li>");
-      }
-      else if (number === "Beep!") {
-        $("#resultList").append("<li class='beep'>"+index+": "+number+"</li>");
-      }
-      else {
-        $("#resultList").append("<li class='normal'>"+index+": "+number+"</li>");
-      }
+
+      printArray(beepBoopArray);
+
+      $(".resultSection").slideDown(1000);
+
+      $(".remixButton").fadeIn(2000);
+
+      $(".remixButton").click(function(){
+        // event.preventDefault();
+        var beepBoopArrayReversed = beepBoopArray.reverse();
+        $("#resultList").children().remove();
+        printArray(beepBoopArrayReversed);
+      });
+
+
+      // WORKING CODE BEFORE TESTS:
+      // beepBoopArray.forEach(function(number,index){
+      //   if (number === "I'm sorry, Dave. I'm afraid I can't do that.") {
+      //     if (nameInput === "") {
+      //       $("#resultList").append("<li class='dave'>"+index+": "+number+"</li>");
+      //     }
+      //     else {
+      //       $("#resultList").append("<li class='dave'>"+index+": "+number.slice(0,11)+nameInput+number.slice(-29,number.length)+"</li>");
+      //     }
+      //   }
+      //   else if (number === "Boop!") {
+      //     $("#resultList").append("<li class='boop'>"+index+": "+number+"</li>");
+      //   }
+      //   else if (number === "Beep!") {
+      //     $("#resultList").append("<li class='beep'>"+index+": "+number+"</li>");
+      //   }
+      //   else {
+      //     $("#resultList").append("<li class='normal'>"+index+": "+number+"</li>");
+      //   }
+      // });
+
+
+
+
+
+
     });
-
-    $(".resultSection").slideDown(1000);
-
   });
-});
