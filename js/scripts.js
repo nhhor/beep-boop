60a2ebf1ccbca04d5a6806e6b9aa3e2b1f98d61c
Noah Horwitz <00noah@gmail.com>
2019-11-01 09:30:54 -0700

    add js function to turn numberInput into range in array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
 // BUSINESS LOGIC
+numberInput = "";
+
+// NUMBER TO RANGE:
+numberArray = [];
+function numberToRange(numberInput) {
+  for (var index = 0; index <= numberInput; index ++) {
+    numberArray.push(index);
+  }
+}
 
 
+function test(numberArray.forEach(number)) {
+  console.log(number);
+});
 
 // USER LOGIC
+
 $(document).ready(function(){
   $("#userForm").submit(function(event){
     event.preventDefault();
 
     var numberInput = $("#userNumber").val();
+
+    numberToRange(numberInput);
+    test();
+
+    console.log(numberArray);
+
     $("#resultList").append("<li>"+numberInput+"</li>");
 
   });
